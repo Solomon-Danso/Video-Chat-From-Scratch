116829e5e434b02be707631a25b1ca38dd7ca863
Solomon <solomondanso2023@gmail.com>
2024-03-29 04:25:59 +0000

    Socket Connection and disconnection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,34 @@
-console.log("Vide")
+import express from "express";
+import http from "http";
+import {Server} from "socket.io"
+import cors from "cors"
+
+
+
+const port = 8080;
+const app = express()
+app.use(cors)
+const server = http.createServer(app)
+const io = new Server(server,{
+    cors:{
+        origin: "*",
+        methods:["GET","POST"]
+    }
+})
+
+io.on("connection",(socket)=>{
+    console.log("user is connected")
+
+    socket.on("disconnect",()=>{
+        console.log("user is disconnected")
+    })
+
+})
+
+
+
+
+
+server.listen(port,()=>{
+    console.log("Server is listening on Port "+port);
+});
